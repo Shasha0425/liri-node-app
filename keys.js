1bb4ac37016c649a5523cc8fdccfa85ca6aded80
Shasha <40842477+Shasha0425@users.noreply.github.com>
2018-10-30 23:54:38 -0400

    updated read me file

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.be30344ad3b1439ca13c8db93a3080a1,
+  secret: process.env.ae3515fec05444e1bca94ea5c5ac7f90
 };
