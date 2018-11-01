c5f0a0900fa1a0b159aa310fbfb5c889392d15b5
Shasha <40842477+Shasha0425@users.noreply.github.com>
2018-10-31 21:14:35 -0400

    added new files

--- liri.js
+++ liri.js
@@ -3,25 +3,13 @@
 // Read and set environment variables
 require("dotenv").config();
 
-// Import the Twitter NPM package.
-var Twitter = require("twitter");
 
-// Import the node-spotify-api NPM package.
 var Spotify = require("node-spotify-api");
-
-// Import the API keys
 var keys = require("./keys");
-
-// Import the request npm package.
 var request = require("request");
-
-// Import the moment npm package.
 var moment = require("moment");
-
-// Import the FS package for read/write.
 var fs = require("fs");
 
-// Initialize the spotify API client using our client id and secret
 var spotify = new Spotify(keys.spotify);
 
 // FUNCTIONS
